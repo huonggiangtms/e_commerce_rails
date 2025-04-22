ad50cc5bbcac228eeb88f712db1ef954591b0bd7
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-22 08:57:40 +0700

    UI admin ( Dashboard )

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root "home#index"
+
+  namespace :admin do
+    root to: 'dashboard#index'
+    resources :users
+  end
 end
