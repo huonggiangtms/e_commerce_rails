220605910743154f49729b58e0943fc6facae3f4
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-24 16:52:50 +0700

    Fix RuboCop offenses in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
 
   root "home#index"
 
   namespace :admin do
-    root to: 'dashboard#index'
+    root to: "dashboard#index"
     resources :users
   end
 end
