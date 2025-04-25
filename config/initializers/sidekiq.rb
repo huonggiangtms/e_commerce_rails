19bedc319da5051a667de860b5711b565f050557
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-25 09:48:32 +0700

    update feature/crawl-product-cron-job: Cronjob crawl

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,13 +4,10 @@ Sidekiq.configure_server do |config|
   schedule_file = "config/sidekiq.yml"
 
   if File.exist?(schedule_file)
-    # Đảm bảo file YAML được load đúng và có khả năng truy cập key theo kiểu symbol hoặc string
     raw_config = YAML.load_file(schedule_file).with_indifferent_access
 
-    # Đảm bảo bạn có phần :schedule đúng trong cấu hình
     Sidekiq.schedule = raw_config[:scheduler][:schedule]
 
-    # Tự động reload lại lịch trình
     Sidekiq::Scheduler.reload_schedule!
   end
 end
