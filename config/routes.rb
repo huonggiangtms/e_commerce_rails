5ba2d5ced76742cc9d178ca9a497b88c39f6a9ff
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-25 09:55:14 +0700

    config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
   root "home#index"
 
   namespace :admin do
-    root to: 'dashboard#index'
+    root to: "dashboard#index"
     resources :users
   end
 end
