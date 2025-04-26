2cd349aeb4a7e35493b12f555a37eba0a1db0f54
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-26 10:49:26 +0700

    feature/crawl-product-ui UI Crawl

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,15 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "dashboard#index"
     resources :users
+    resources :crawl_jobs
+    resources :crawled_products do
+      member do
+        patch :approve
+      end
+      collection do
+        get :manual_crawl
+        post :crawl_manual
+      end
+    end
   end
 end
