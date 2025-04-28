5a99ee859f2229de66c7785dcb9c2051a065b562
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-28 10:16:31 +0700

    feature/manage-users RUD User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :check_admin, if: :admin_page?
+
+  private
+
+  def check_admin
+    unless current_user && current_user.has_role?(:admin)
+      redirect_to root_path, alert: "You do not have permission to access this page."
+    end
+  end
+
+  def admin_page?
+    request.path.start_with?("/admin")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
+  end
 end
