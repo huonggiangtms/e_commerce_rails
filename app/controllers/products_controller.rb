0895be69cc60b8114b0fec60429f626d67f5dd11
tms-giangnguyen <giang.nguyen1@tomosia.com>
2025-04-29 14:12:50 +0700

    fix rubocop

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,17 +4,15 @@ class ProductsController < ApplicationController
     @products = Product.all
 
   @sort_options = [
-    ["Mặc định", ""],
-    ["Giá tăng dần", ""],
-    ["Giá giảm dần", ""],
+    [ "Mặc định", "" ],
+    [ "Giá tăng dần", "" ],
+    [ "Giá giảm dần", "" ]
   ]
-
   end
 
   def show
     @product = Product.find(params[:id])
     @related_products = Product.where(category_id: @product.category_id).where.not(id: @product.id).limit(8)
-
   end
 
   private
@@ -22,5 +20,4 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:name, :description, :price, :stock, :image_url, :category_id)
   end
-
 end
